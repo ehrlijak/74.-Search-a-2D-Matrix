f0c4f07b0a3f7556c43caeb3f4178b08f05644d6
jakub <jakubehrlich6@gmail.com>
2026-03-16 22:48:55 +0100

    Last testcase has problems, bruteforce does not work.

--- index.js
+++ index.js
@@ -58,29 +58,44 @@ function rowSearch(arr, target, pointer, lowBound, highBound) {
         pointer = Math.floor((highBound + lowBound )/2);
         return rowSearch(arr, target, pointer, lowBound, highBound);
     }
-    else if (arr[pointer][0] < target && arr[pointer][arr[0].length -1 ] < target) {
+    else if (arr[pointer][0] < target */&& arr[pointer][arr[0].length -1 ] < target*/) {
         lowBound = pointer + 1;
 
         pointer = Math.floor((highBound + lowBound )/2);
         return rowSearch(arr, target, pointer, lowBound, highBound);
     }
-    else {
+    else if (arr[pointer][0] === target) {
         return pointer;
     }
-
-
+    else if (arr[pointer][1] === target) {
+        return pointer;
     }
+    else {
+        return false
+    }
+
+
+
+}
 
     var pointer = Math.floor((matrix.length -1 ) / 2);
     var row = rowSearch(matrix, target, pointer, 0, matrix.length - 1);
+    if(row === false || row < 0) {
+        return false;
+    }
     pointer = Math.floor((matrix[0].length -1 ) / 2);
     var column = columnSearch(matrix, target, pointer, row, 0, matrix[0].length - 1);
 
-
+     if(matrix[row][column] === target) {
+         return true;
+     }
+     else {
+         return false;
+     }
 
 
     return `Row is : ${row} \n Column: ${column}\n`;
 };
 
 
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 30))
+console.log(searchMatrix([[1],[3]], target = 3))
